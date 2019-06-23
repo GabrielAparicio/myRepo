6973373d546cde347f1157fa1004a67ebf546b98
GabrielAparicio <sgaparicio22@gmail.com>
2019-06-23 16:21:47 -0500

    Change made from web page in brach2

--- file1.cpp
+++ file1.cpp
@@ -6,5 +6,6 @@ int main()
   cout<<"This is my branch1 branch!";
   cout<<"This is my second modification on branch1 !"<<endl;
   cout<<"Change made in branch2"<<endl;
+  cout<<"Change in branch2 made from web page"<<endl;
   return 0;
 }
